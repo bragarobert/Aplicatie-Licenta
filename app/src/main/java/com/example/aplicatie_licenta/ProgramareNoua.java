01e7693d91ab21d4c4dfab85026a955bf52bbd3a
bragarobert <72302354+bragarobert@users.noreply.github.com>
2021-06-10 00:01:04 +0300

    First commit

--- app/src/main/java/com/example/aplicatie_licenta/ProgramareNoua.java
+++ app/src/main/java/com/example/aplicatie_licenta/ProgramareNoua.java
@@ -2,9 +2,12 @@ package com.example.aplicatie_licenta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -13,7 +16,10 @@ import android.widget.TextView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class ProgramareNoua extends AppCompatActivity {
 
@@ -35,7 +41,7 @@ public class ProgramareNoua extends AppCompatActivity {
 
         databaseReference= FirebaseDatabase.getInstance().getReference();
         initViews();
-
+        selectareData();
 
     }
 
@@ -47,5 +53,28 @@ public class ProgramareNoua extends AppCompatActivity {
         ibAlegeData = findViewById(R.id.btn_alege_data);
         lvOreDisponibile = findViewById(R.id.lv_ore_disponibile);
         btnRezerva = findViewById(R.id.btn_rezerva_programare);
+    }
+
+    private void selectareData(){
+        DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+                tvDataProgramare.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        };
+
+        ibAlegeData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog( ProgramareNoua.this,onDateSetListener,
+                        calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.getDatePicker().setMinDate(new Date().getTime());
+                datePickerDialog.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/aplicatie_licenta/autentificare_inregistrare/StartActivity.java
+++ app/src/main/java/com/example/aplicatie_licenta/autentificare_inregistrare/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.aplicatie_licenta.autentificare_inregistrare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_start);
